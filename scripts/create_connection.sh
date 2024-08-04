a4c9944cd1aa88d0bbb38b0937dbd09e1c35abe8
JordanAmaralVicente <jordanamaralvicente@gmail.com>
2024-08-04 15:09:29 +0000

    chore(docker/email): setup email on dag fail and add api connection on server startup

--- scripts/create_connection.sh
+++ scripts/create_connection.sh
@@ -7,3 +7,9 @@ airflow connections add data_warehouse \
     --conn-schema datawarehouse
 
 # postgresql://$DW_LOGIN:$DW_PASSWORD@datawarehouse:5432/datawarehouse
+
+airflow connections add ahoy_viewer_ti \
+    --conn-type http \
+    --conn-host app.alunos.me \
+    --conn-schema https \
+    --conn-extra '{"token": "G5n2w1lm*eJF$UukF5c^bN5Re#"}'
